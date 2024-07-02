516e79c6b3f74f75bbc68235fddacf430bcd44d3
Serkan BÜTÜN <63592375+Serkan-py@users.noreply.github.com>
2024-07-02 16:53:14 +0300

    Update ders8.py
    Lokal Haraket Ettirme icin Kaynak Ekleme Yapildi

--- ders8.py
+++ ders8.py
@@ -1,3 +1,19 @@
+"""
+Arkadaslar merhaba, bu python dosyasi insansiz bir araci kuzey(ileri), guney(geri), saga(dogu), sola(bati), yukari ve asagi yonlu 
+haraket ettirmeniz icin hazirlandi. master.mav.set_position_target_local_ned_send komutundaki bazi parametreleri degistirerek gidilecek yonun merkezini,
+gidilecek hizi veya sureyi ayarlayabilirsiniz.
+iste yararlanacaginiz kaynaklarin linkleri:
+
+https://ardupilot.org/dev/docs/copter-commands-in-guided-mode.html#copter-commands-in-guided-mode-set-position-target-local-ned
+
+https://mavlink.io/en/messages/common.html#SET_POSITION_TARGET_LOCAL_NED
+
+bu iki sitede bu komutun nasil kullanilacagina yonelik detayli anlatim yapilmis zaten videoda anlatirken de bu kaynaklardan yararlaniyorum. hangi 
+parametrenin ne ise yaradigini tek tek gorup anlayabilirsiniz. yine anlamadiginiz veya yapamadiginiz bir sey olursa bana mail veya linkedin 
+uzerinden ulasabilirsiniz. Iletisim bilgilerim readme dosyasinda bulunmakta, iyi calismalar dilerim.
+
+"""
+
 from pymavlink import mavutil
 import sys
 master = mavutil.mavlink_connection('127.0.0.1:14550')
@@ -9,13 +25,13 @@ print("baglandi")
 
 x = 5
 y = 10
-alt = -10
+alt = -10 # yukari yon icin (-) asagi yon icin (+) deger verilmeli.
 
 
 master.mav.set_position_target_local_ned_send(
     0, master.target_system, master.target_component,
     mavutil.mavlink.MAV_FRAME_LOCAL_NED,
-    0b110111111000,
+    0b110111111000, 
     x,
     y,
     alt,
